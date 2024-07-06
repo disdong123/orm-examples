8740f99a5bd380cf6e0975c314c9c08ba7e66e19
disdong123 <gksrlfw@gmail.com>
2024-07-06 14:39:37 +0900

    feat: remove unused file

--- orm-examples-server/src/main/kotlin/kr/disdong/orm/examples/server/domain/user/exception/UserNotFound.kt
+++
@@ -1,5 +0,0 @@
-package kr.disdong.orm.examples.server.domain.user.exception
-
-import kr.disdong.orm.examples.common.exception.NotFoundException
-
-class UserNotFound(private val userId: Long) : NotFoundException("$userId 유저를 찾을 수 없습니다.")
